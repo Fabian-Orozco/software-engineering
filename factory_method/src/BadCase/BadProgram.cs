b3c1b54b2796066c3988a085d561b5723318271c
Fabian-Orozco <63373053+Fabian-Orozco@users.noreply.github.com>
2024-02-06 20:12:31 -0600

    upload presentation

---
+++ factory_method/src/BadCase/BadProgram.cs
@@ -0,0 +1,28 @@
+﻿// Problem
+// Basado en: Dive Into Design Patterns [Alexander Shvets]
+using System;
+
+namespace BadCase {
+
+    class Avion {
+        public string Operation() {
+            return "{Soy un avion}";
+        }
+    }
+
+    class Carro {
+        public string Operation() {
+            return "{Soy un carro}";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Avion Avion = new Avion();
+            Carro Carro = new Carro();
+            Console.WriteLine(Avion.Operation());
+            Console.WriteLine(Carro.Operation());
+        }
+    }
+}
+
---
+++ factory_method/src/GoodCase/GoodProgram.cs
@@ -0,0 +1,57 @@
+﻿// Factory Method Design Pattern
+// Basado en: Dive Into Design Patterns [Alexander Shvets]
+using System;
+
+namespace GoodCase {
+
+    abstract class Creator {
+        public abstract IVehiculo FactoryMethod();
+
+        public string SomeOperation() {
+            var product = FactoryMethod();
+            var result = "Creador: el mismo código sirve para crear " + product.Operation();
+            return result;
+        }
+    }
+
+    class CreatorAvion: Creator {
+        public override IVehiculo FactoryMethod() {
+            return new Avion();
+        }
+    }
+
+    class CreatorCarro : Creator {
+        public override IVehiculo FactoryMethod() {
+            return new Carro();
+        }
+    }
+
+    public interface IVehiculo {
+        string Operation();
+    }
+
+    class Avion : IVehiculo {
+        public string Operation() {
+            return "{Soy un avion}";
+        }
+    }
+
+    class Carro : IVehiculo {
+        public string Operation() {
+            return "{Soy un carro}";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            List<Creator> creators = new List<Creator>();
+            creators.Add(new CreatorAvion());
+            creators.Add(new CreatorCarro());
+
+            foreach (Creator creator in creators) {
+                Console.WriteLine(creator.SomeOperation());
+            }
+        }
+    }
+}
+
